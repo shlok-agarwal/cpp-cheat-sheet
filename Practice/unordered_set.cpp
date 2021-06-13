65f0f6b9a30f175f2a300937ee519a097b67dbab
shlok <ssagarwal@wpi.edu>
2021-06-13 16:11:23 -0400

    improved set example

--- Practice/unordered_set.cpp
+++ Practice/unordered_set.cpp
@@ -17,16 +17,27 @@ int main()
 {
     // use this for ordered set
     // set<int> uset = {1,5,7,2,7,5,2};
-
     unordered_set<int> uset = {1,5,7,2,7,5,2};
-    auto search = uset.find(10); // returns address pointer
+    
+    int elm = 5;
+    auto search = uset.find(elm); // returns address pointer
     if(search != uset.end())
         cout << "Found " << *search << "\n";
-    else cout << "Not Found \n";
+    else cout << "Not Found " << elm << " \n";
+    
+    if(uset.count(elm) > 0)
+        cout<<"Found and count is " << uset.count(elm) << "\n";
+    else cout << "Not Found " << elm << " \n";
 
     for(auto &elm: uset)
     {
         cout<< elm <<"\t";
     }
+    cout<<"\n";
+
+    // if ordered, you can use the order to print the elements before or after the iterator
+    auto itr = uset.find(elm);
+    for(auto i = itr; i!=uset.end(); i++)
+        cout<< *i << "\t";
     cout<<"\n";
 }
