92789b98366707d0263d961a1cd5e038e3b917a5
shlok <ssagarwal@wpi.edu>
2021-06-13 15:21:13 -0400

    multiset

---
+++ Practice/multiset.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <functional>
+#include <string>
+#include <set>
+
+using namespace std;
+
+// Use multiset when you want ordered dataset with duplicates
+// Insertion, removal and search happens to logarithmic complexity
+
+int main()
+{
+    multiset<int, greater<>> M = {2,3,4,5,2,1,5};
+    for(auto &elm : M)
+        cout<< elm << "\t";
+    cout << "\n";
+
+    // search and print using the pointer
+    auto itr = M.find(3);
+    for (auto i = itr; i != M.end(); i++)
+    {
+        cout << *i <<"\t";
+    }
+    cout << "\n";
+    
+    // count and search
+    int count = M.count(10);
+    cout << "10 occurs " << count << " times";
+    // Can also be used to check if 5 exists. 
+
+    return 0;
+}
