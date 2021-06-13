02a79a7ccb93443be97484daf0677273a630cd3c
shlok <ssagarwal@wpi.edu>
2021-06-13 15:56:35 -0400

    improved map example

--- Practice/map.cpp
+++ Practice/map.cpp
@@ -1,5 +1,6 @@
 
 #include <map>
+#include <unordered_map>
 #include <iostream>
 
 using namespace std;
@@ -12,13 +13,34 @@ using namespace std;
 
 int main()
 {
+    // ORDERED O(log n)
     map <string, int, less<>> M; // add greater to change sort order; default is less
     M["cat"] = 1;
     M["dog"] = 2;
+    // this will overwrite the previous key. To not overwrite, check if already exists
+    M["dog"] = 3; 
     M.insert(make_pair("elephant", 4));
 
     for(auto &elm: M)
         cout<<elm.first <<"\t" << elm.second <<"\n";
+    cout <<"\n";
+    
+    // UNORDERED O(1)
+    unordered_map<string, int> N; 
+    N["cat"] = 1;
+    N["dog"] = 2;
+    // this will overwrite the previous key. To not overwrite, check if already exists
+    N["dog"] = 3;
+    N.insert(make_pair("elephant", 4));
 
+    for(auto &elm: N)
+        cout<<elm.first <<"\t" << elm.second <<"\n";
+
+    // SEARCH is the same for ordered and unordered.
+    if(N.find("cat") != N.end())
+        cout<<M["cat"];
+    if(N.count("cat") > 0)
+        cout<<N["cat"] << "\n";
+    
     return 0;
 }
